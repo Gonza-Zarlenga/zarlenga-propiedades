87d335e5673ea2bda3d5acb227b9d66b9d6aaccf
Gonza-Zarlenga <gzarlenga95@gmail.com>
2024-05-27 20:53:36 -0300

    sumo notfounf

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 
 // enrutadores
 
 const mainRouter = require ("./routes/mainRouter.js");
 
+//........///
+app.set ('views', path.join(__dirname,"views"));
+app.set ('view engine', 'ejs');
+
 // Recursos estaticos 
 
 app.use (express.static('public'));
+app.use((req,res,next)=>{
+    res.status(404).render("notFound")
+});
 
 // usando enrutadores importado
 
